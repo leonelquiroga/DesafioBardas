e2f75da9c212253bf6fa39f0266754da7bffdd5a
leonelquiroga <leito.quiroga.94@gmail.com>
2019-05-20 15:46:43 -0300

    first commit

---
+++ views/site/index.php
@@ -0,0 +1,134 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Proyecto Web';
+?>
+<div class="site-index">
+    <div class="container">
+    <!-- Seccion inicio -->
+    <div class="logo">
+    <div class="jumbotron">
+        <h2>Proyecto web</h2>
+        <p> img Logo</p>
+        <!--<p class="lead">You have successfully created your Yii-powered application.</p>
+        -->
+        <ul class="list-group list-inline">
+            <li class="list-inline-item"><a href="#">Link</a></li>
+            <li class="list-inline-item"><a href="#">Link</a></li>
+            <li class="list-inline-item"><a href="#">Link</a></li>
+            <li class="list-inline-item"><a href="#">Link</a></li>
+            <li class="list-inline-item"><a href="#">Link</a></li>
+            
+        </ul>
+        <!--<p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        -->
+    </div>
+    </div>    
+
+    <div class="body-content">
+        <!-- Seccion slogan -->
+        <div class="slogan">
+            <div class="jumbotron">
+                <h1>"Tu residuo, tu responsabilidad"</h1>
+                
+                <p class="text-justify"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+                
+            </div>
+            
+        </div>
+        
+        <div class="row">
+            <div class="col-lg-6">
+                <h3>Requisitos Carrera</h3>
+                
+                <ul class="list-group">
+                    <li class="list-group-item">Quien participa</li>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                   </p>
+                   <li class="list-group-item">¿Como ganar?</li>
+                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                   </p>
+                </ul>
+                
+            </div>
+            <div class="col-lg-6">
+                <h3>Premiacion</h3>
+                
+                <p class="list-group-item">Categorias</p>
+                
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="#">Conoce mas</a></p>
+            </div>
+        </div>
+        <!-- Seccion sponsor -->
+        <div class="sponsor">
+            <div class="jumbotron">
+                <h1>Programa de sponsoreo</h1>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+                </div>
+                <div class="col-lg-6">
+                    <h3>"Buscamos y necesitamos de tu apoyo para costear este desafio."</h3>
+                </div>
+                
+            </div>
+        </div>
+        <br><br>
+        <!-- Seccion imagen/texto-->
+        <div class="info">
+            <div class="row">
+                <div class="col-lg-8">
+                    <img src="../views/img/cuadrado.jpg" width="650" height="250" alt="cuadrado"/>    
+                </div>
+                <div class="col-lg-4">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <!-- Seccion cuenta regresiva / formulario -->
+        
+        <div class="row">
+            <div class="col-lg-5">
+                <iframe width="457" height="235" src="https://w2.countingdownto.com/2473794" frameborder="0"></iframe>
+            </div>
+            <div class="col-lg-7">
+                <form>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Direccion de Email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">Nosotros jamás te pediremos que compartas esto.</small>
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
+            </div>
+        
+
+    </div>
+</div>
+</div>
